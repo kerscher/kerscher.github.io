6381326e3ad978ef4d7f01bfcd788bc8ca4d9e6a
Yghor Kerscher <kerscher@outlook.com>
2018-05-05 16:08:42 +0200

    Split out `Rules` from `main`

--- source/Main.hs
+++ source/Main.hs
@@ -1,52 +1,58 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, UnicodeSyntax #-}
+
 import Hakyll
 
 configuration :: Configuration
-configuration =
-  defaultConfiguration
-  { providerDirectory    = "source"
-  , destinationDirectory = "docs"
-  , storeDirectory       = "cache"
-  , tmpDirectory         = "cache/tmp"
-  }
-  
+configuration = defaultConfiguration { providerDirectory    = "source"
+                                     , destinationDirectory = "docs"
+                                     , storeDirectory       = "cache"
+                                     , tmpDirectory         = "cache/tmp"
+                                     }
+
 main :: IO ()
-main = hakyllWith configuration rules
+main = hakyllWith configuration $ do
+  templates
+  images
+  style
+  posts
+  index
 
-rules :: Rules ()
-rules = do
-  match "templates/*" $ compile templateCompiler
-  
-  match "images/*" $ do
-    route   idRoute
-    compile copyFileCompiler
+templates :: Rules ()
+templates = match "templates/*" $ compile templateCompiler
 
-  match "style/*" $ do
-    route   idRoute
-    compile copyFileCompiler
-  
-  match "posts/*" $ do
-    route $ setExtension "html"
-    compile $ pandocCompiler
-      >>= loadAndApplyTemplate "templates/post.html"    postCtx
-      >>= loadAndApplyTemplate "templates/default.html" postCtx
+images :: Rules ()
+images = match "images/*" $ do
+  route idRoute
+  compile copyFileCompiler
+
+style :: Rules ()
+style = match "style/*" $ do
+  route idRoute
+  compile copyFileCompiler
+
+posts :: Rules ()
+posts = match "posts/*" $ do
+  route $ setExtension "html"
+  compile
+    $   pandocCompiler
+    >>= loadAndApplyTemplate "templates/post.html"    postCtx
+    >>= loadAndApplyTemplate "templates/default.html" postCtx
+    >>= relativizeUrls
+
+index :: Rules ()
+index = match "index.html" $ do
+  route idRoute
+  compile $ do
+    posts' <- recentFirst =<< loadAll "posts/*"
+    let indexContext =
+          listField "posts" postCtx (return posts')
+            `mappend` constField "title"       "Yghor Kerscher"
+            `mappend` constField "landingPage" mempty
+            `mappend` defaultContext
+    getResourceBody
+      >>= applyAsTemplate indexContext
+      >>= loadAndApplyTemplate "templates/default.html" indexContext
       >>= relativizeUrls
 
-  match "index.html" $ do
-    route idRoute
-    compile $ do
-      posts <- recentFirst =<< loadAll "posts/*"
-      let indexContext =
-            listField  "posts" postCtx (return posts) `mappend`
-            constField "title" "Yghor Kerscher"       `mappend`
-            constField "landingPage" mempty           `mappend`
-            defaultContext           
-      getResourceBody
-        >>= applyAsTemplate indexContext
-        >>= loadAndApplyTemplate "templates/default.html" indexContext
-        >>= relativizeUrls
-
 postCtx :: Context String
-postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+postCtx = dateField "date" "%B %e, %Y" `mappend` defaultContext
